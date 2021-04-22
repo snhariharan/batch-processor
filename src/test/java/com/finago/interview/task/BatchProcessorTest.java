e56c1c9d8b24581b44169aa9268b5c1e8ea2e027
Hariharan Natarajan <hariharan.natarajan@snowfox.ai>
2021-04-22 23:45:55 +0300

    add sample test case

--- src/test/java/com/finago/interview/task/BatchProcessorTest.java
+++ src/test/java/com/finago/interview/task/BatchProcessorTest.java
@@ -2,9 +2,7 @@ package com.finago.interview.task;
 
 import com.finago.interview.task.service.DirectoryWatchService;
 import junit.framework.TestCase;
-import org.junit.After;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.File;
@@ -13,32 +11,84 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Unit test for BatchProcessor.
  */
 public class BatchProcessorTest extends TestCase {
 
-    @Test
-    public void testApp() throws IOException {
-        File rootDirectory =  new File("data/in/");
-        File directory = new File("src/test/resources/");
+    @Before
+    protected void setUp() throws InterruptedException {
+        File rootDirectory = new File("data/in/");
+        File outDirectory = new File("data/out/");
+        File directory = new File("src/test/root/");
         final String rootPath = directory.getAbsolutePath();
         Path watchDirectory = Paths.get(rootPath + "/in/");
-        new java.util.Timer().schedule(
-                new java.util.TimerTask() {
-                    @Override
-                    public void run() {
-                        try {
-                            Files.move(Paths.get(rootDirectory.getAbsolutePath() + "/"), watchDirectory, StandardCopyOption.REPLACE_EXISTING);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                },
-                5000
-        );
-        new DirectoryWatchService(directory.getAbsolutePath() + "/").processEvents();
+        moveFiles(rootDirectory, watchDirectory);
+        Killable task = new Killable();
+        Thread thread = new Thread(task);
+        thread.start();
+        TimeUnit.SECONDS.sleep(10);
+        task.kill();
+        thread.interrupt();
     }
 
+    @Test
+    public void testsOutputFolder() throws InterruptedException {
+        File outDirectory0 = new File("data/out/0");
+        File outDirectory1 = new File("data/out/1");
+        assertTrue(outDirectory0.exists());
+        assertTrue(outDirectory1.exists());
+        String[] pathnames = outDirectory0.list();
+        assert pathnames != null;
+        assertEquals(pathnames.length, 8);
+        File firstIn0 = new File("data/out/0/8842");
+        assertTrue(firstIn0.exists());
+        String[] files = firstIn0.list();
+        assert files != null;
+        assertEquals(files.length, 4);
+    }
+
+    private void moveFiles(File rootDirectory, Path watchDirectory) {
+        new Timer().schedule(
+            new TimerTask() {
+                @Override
+                public void run() {
+                    try {
+                        Files.move(Paths.get(rootDirectory.getAbsolutePath() + "/"), watchDirectory, StandardCopyOption.REPLACE_EXISTING);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            },
+            5000
+        );
+    }
+
+    static class Killable implements Runnable {
+        private volatile boolean killed = false;
+
+        public void run() {
+            while (!killed) {
+                try {
+                    doOnce();
+                } catch (InterruptedException | IOException ex) {
+                    killed = true;
+                }
+            }
+        }
+
+        public void kill() {
+            System.out.println("Killing service called");
+            killed = true;
+        }
+
+        private void doOnce() throws InterruptedException, IOException {
+            File directory = new File("src/test/root/");
+            new DirectoryWatchService(directory.getAbsolutePath() + "/").processEvents();
+        }
+    }
 }
